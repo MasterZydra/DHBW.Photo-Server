433b1cc35f3fb91750251172c39072faaf20df05
David Hein <david-hein196@web.de>
2020-11-18 22:41:52 +0100

    Add struct ImageManager

---
+++ internal/image/imagemanager.go
@@ -0,0 +1,33 @@
+package image
+
+import "strings"
+
+type ImageManager struct {
+	images []*Image
+}
+
+func (im *ImageManager) AddImage(image *Image) {
+	im.images = append(im.images, image)
+}
+
+func (im *ImageManager) Sort() {
+	// Do something
+}
+
+func (im *ImageManager) GetPhoto(name string) *[]byte {
+	for _, i := range im.images {
+		if strings.Compare(i.Name, name) == 0 {
+			return &[]byte{}
+		}
+	}
+	return nil
+}
+
+func (im *ImageManager) GetThumbnail(name string) *[]byte {
+	for _, i := range im.images {
+		if strings.Compare(i.Name, name) == 0 {
+			return &[]byte{}
+		}
+	}
+	return nil
+}

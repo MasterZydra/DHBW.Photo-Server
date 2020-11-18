978578b02d99623515251c43629cabbde06bf05a
David Hein <david-hein196@web.de>
2020-11-18 22:17:26 +0100

    Add simple webserver

---
+++ cmd/backend/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", mainHandler)
+	log.Fatalln(http.ListenAndServeTLS(":4443", "cert.pem", "key.pem", nil))
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	responseString := "<html><body>Hallo</body></html>"
+	w.Write([]byte(responseString))
+}

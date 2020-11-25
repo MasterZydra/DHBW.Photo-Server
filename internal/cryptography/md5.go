8da41040a88f12b7e96df6828fe42470a48e168a
David Hein <david-hein196@web.de>
2020-11-25 20:47:17 +0100

    Change return type of HashImage Add HashToImage func

--- internal/cryptography/md5.go
+++ internal/cryptography/md5.go
@@ -1,7 +1,15 @@
 package cryptography
 
-import "crypto/md5"
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
 
-func HashImage(image []byte) [md5.Size]byte {
-	return md5.Sum(image)
+func HashImage(image *[]byte) []byte {
+	hash := md5.Sum(*image)
+	return hash[:]
+}
+
+func HashToString(hash []byte) string {
+	return hex.EncodeToString(hash)
 }

b388e99923582feec2477f10a6a8ff1ad474afea
David Hein <david-hein196@web.de>
2020-11-25 19:28:30 +0100

    Add func to generate new image object

--- internal/image/image.go
+++ internal/image/image.go
@@ -1,9 +1,16 @@
 package image
 
+// import "../cryptography"
+
 type Image struct {
 	Name string
 	Date string
 	Hash string
+}
+
+func NewImage(name string, date string) *Image {
+	// ToDo - Implement
+	return &Image{}//Name: name, Date: date} //, Hash: cryptography.HashImage()}
 }
 
 func (i *Image) GetThumb() *[]byte {

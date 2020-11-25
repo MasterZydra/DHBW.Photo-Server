219587a131eba9f1aa891ed5cb7b198ed3cfd2d9
David Hein <david-hein196@web.de>
2020-11-25 21:29:27 +0100

    Starting implementing read func

--- internal/image/contentlist.go
+++ internal/image/contentlist.go
@@ -1,6 +1,25 @@
 package image
 
+import "encoding/csv"
+import "strings"
+import "fmt"
+
 func ReadContent(user string) *ImageManager {
+	reader := csv.NewReader(strings.NewReader(""))
+	reader.Comma = '|'
+
+	images, err := reader.ReadAll()
+	if err != nil {
+		// Error stuff
+		fmt.Println(err)
+	}
+
+	for _, img := range images {
+		fmt.Println("Name", img[0])
+		fmt.Println("Datum", img[1])
+		fmt.Println("Hash", img[1])
+	}
+
 	return &ImageManager{}
 }
 

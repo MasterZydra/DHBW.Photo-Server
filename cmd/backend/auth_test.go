d762f0bd454df9183176e3b68f6609a583554ba3
jones1008 <jones@fluhrers.de>
2020-12-14 22:14:56 +0100

    added two tests

--- cmd/backend/auth_test.go
+++ cmd/backend/auth_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,7 +12,7 @@ func createServer(auth AuthenticatorFunc) *httptest.Server {
 	return httptest.NewServer(
 		AuthWrapper(auth,
 			func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprintln(w, "Hello client")
+				fmt.Fprintln(w, "string from server")
 			}))
 }
 
@@ -27,5 +28,53 @@ func TestWithoutPassword(t *testing.T) {
 	}
 }
 
+func doRequestWithPassword(url string) (*http.Response, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if req == nil || err != nil {
+		return nil, err
+	}
+	req.SetBasicAuth("<username>", "<password>")
+	res, err := client.Do(req)
+	return res, nil
+}
+
 func TestWithCorrectPassword(t *testing.T) {
+	var receivedName, receivedPwd string
+	server := createServer(func(name, pwd string) bool {
+		receivedName = name
+		receivedPwd = pwd
+		return true
+	})
+	defer server.Close()
+	res, _ := doRequestWithPassword(server.URL)
+
+	if res.StatusCode != http.StatusOK || receivedName != "<username>" || receivedPwd != "<password>" {
+		t.Error("Authentication with password didn't work, but it should")
+	} else {
+		body, _ := ioutil.ReadAll(res.Body)
+		if string(body) != "string from server\n" {
+			t.Error("wrong message in body from server")
+		}
+	}
+}
+
+func TestWithWrongPassword(t *testing.T) {
+	var receivedName, receivedPwd string
+	server := createServer(func(name, pwd string) bool {
+		receivedName = name
+		receivedPwd = pwd
+		return false
+	})
+	defer server.Close()
+	res, _ := doRequestWithPassword(server.URL)
+
+	if res.StatusCode != http.StatusUnauthorized || receivedName != "<username>" || receivedPwd != "<password>" {
+		t.Error("Authentication with wrong password did not fail")
+	} else {
+		body, _ := ioutil.ReadAll(res.Body)
+		if string(body) != http.StatusText(http.StatusUnauthorized)+"\n" {
+			t.Error("wrong message in body from server")
+		}
+	}
 }

c78670cc608fb4a944f70591e82111146850a251
jones1008 <jones@fluhrers.de>
2020-12-28 10:36:43 +0100

    added html templates + template variables + run config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,23 +8,26 @@ import (
 	"path/filepath"
 )
 
-func main() {
-	port := "4443"
+const port = "4443"
 
+type templateData struct {
+	BackendHost string
+}
+
+func main() {
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/public/", http.StripPrefix("/public/", fs))
 
 	http.HandleFunc("/", serverTemplate)
 
-	log.Println("web listening on " + port)
+	log.Println("web listening on https://localhost:" + port)
 	log.Fatalln(http.ListenAndServeTLS(":"+port, "cert.pem", "key.pem", nil))
 }
 
 func serverTemplate(w http.ResponseWriter, r *http.Request) {
 	wd, _ := os.Getwd()
-	dir := filepath.Join(wd, "cmd", "web")
-	layout := filepath.Join(dir, "templates", "layout.html")
-	publicFile := filepath.Join(dir, "public", filepath.Clean(r.URL.Path))
+	layout := filepath.Join(wd, "templates", "layout.html")
+	publicFile := filepath.Join(wd, "public", filepath.Clean(r.URL.Path))
 
 	siteStat, err := os.Stat(publicFile)
 	if err != nil && os.IsNotExist(err) {
@@ -44,9 +47,15 @@ func serverTemplate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = tmpl.ExecuteTemplate(w, "layout", nil)
+	err = tmpl.ExecuteTemplate(w, "layout", getTemplateData())
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, http.StatusText(500), 500)
+	}
+}
+
+func getTemplateData() templateData {
+	return templateData{
+		BackendHost: "https://localhost:3000/",
 	}
 }

3dfaf9e3865cbc127b997985f658404fb157f3ba
David Hein <david-hein196@web.de>
2020-12-28 12:18:55 +0100

    Implementation to read and write content file

--- internal/image/contentlist.go
+++ internal/image/contentlist.go
@@ -1,28 +1,74 @@
 package image
 
-import "encoding/csv"
-import "strings"
+import (
+	"encoding/csv"
+	"os"
+	"path"
+)
 import "fmt"
 
+var usercontent = "content.csv"
+
+// Read "content.csv" for given user. The user has to be equal to the folder name.
+// It returns an initialized ImageManager struct.
 func ReadContent(user string) *ImageManager {
-	reader := csv.NewReader(strings.NewReader(""))
+	// Open file
+	csvFile, err := os.Open(path.Join(user, usercontent))
+	if err != nil {
+		// Error stuff
+		fmt.Println(err)
+	}
+
+	// Initialize reader
+	reader := csv.NewReader(csvFile)
 	reader.Comma = '|'
 
+	// Read file
 	images, err := reader.ReadAll()
 	if err != nil {
 		// Error stuff
 		fmt.Println(err)
 	}
 
+	// Store all data in ImageManager struct
+	imageManager := &ImageManager{}
 	for _, img := range images {
-		fmt.Println("Name", img[0])
-		fmt.Println("Datum", img[1])
-		fmt.Println("Hash", img[1])
+		image := Image{Name: img[0], Date: img[1], Hash: img[2]}
+		imageManager.AddImage(&image)
 	}
 
-	return &ImageManager{}
+	return imageManager
 }
 
+// Write "content.csv" for given user. The user has to be equal to the folder name.
 func WriteContent(user string, imgs *ImageManager) error {
+	// Create new file
+	f, err := os.Create(path.Join(user, usercontent))
+	if err != nil {
+		return err
+	}
+
+	// Initialize writer
+	writer := csv.NewWriter(f)
+	writer.Comma = '|'
+
+	// Build array that will be stored in CSV format
+	var imgArray [][]string
+	for _, img := range imgs.images {
+		imgArray = append(imgArray, []string{img.Name, img.Date, img.Hash})
+	}
+
+	// Write file
+	err = writer.WriteAll(imgArray)
+	if err != nil {
+		return err
+	}
+	writer.Flush()
+
+	// Close file
+	err = f.Close()
+	if err != nil {
+		return err
+	}
 	return nil
 }

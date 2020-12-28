abd99f8bfaaebb679c0085d3476fd8dd6ca3d6e7
David Hein <david-hein196@web.de>
2020-12-28 15:21:26 +0100

    Move UploadImage to own file

--- internal/image/image.go
+++ internal/image/image.go
@@ -1,21 +1,9 @@
 package image
 
-import "DHBW.Photo-Server/internal/cryptography"
-
 type Image struct {
 	Name string
 	Date string
 	Hash string
-}
-
-type UploadImage struct {
-	Image
-	Raw []byte
-}
-
-func NewUploadImage(name string, raw []byte) UploadImage {
-	hash := cryptography.HashToString(cryptography.HashImage(&raw))
-	return UploadImage{Image: Image{Name: name, Hash: hash}, Raw: raw}
 }
 
 func NewImage(name string, date string) *Image {

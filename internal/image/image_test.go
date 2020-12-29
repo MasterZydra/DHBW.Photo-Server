d787f3c509962ca6e7c3899ed50e424749ceac30
David Hein <david-hein196@web.de>
2020-12-29 13:59:51 +0100

    Add test

---
+++ internal/image/image_test.go
@@ -0,0 +1,19 @@
+package image
+
+import (
+	"testing"
+)
+
+func TestNewImage(t *testing.T) {
+	img := NewImage("img1", "2020-11-20", "d41d8cd98f00b204e9800998ecf8427e")
+
+	if img.Name != "img1" ||
+		img.Hash != "d41d8cd98f00b204e9800998ecf8427e" ||
+		img.Date.Format("2006-01-02") != "2020-11-20" {
+		t.Errorf("Image object not filled correctly")
+	}
+}
+
+func TestNewImage_Invalid(t *testing.T) {
+	// ToDo Implement
+}

8f9e18e0d5d2fbe36a852a1c6549692513c2a026
David Hein <david-hein196@web.de>
2020-12-31 11:53:31 +0100

    Add backend logic

---
+++ cmd/backend/image.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"DHBW.Photo-Server/internal/image"
+)
+
+var imageManagers = make(map[string]*image.ImageManager)
+
+func getImageManager(user string) *image.ImageManager {
+	imgman, exists := imageManagers[user]
+	if !exists {
+		imgman = image.NewImageManager(user)
+		imageManagers[user] = imgman
+	}
+	return imgman
+}
+
+func UploadImage(user, name, creationDate string, raw []byte) string {
+	upimg := image.NewUploadImage(name, creationDate, raw)
+
+	imgman := getImageManager(user)
+
+	if imgman.Contains(&upimg) {
+		return "Image is already stored"
+	}
+	imgman.AddImageUpload(&upimg)
+	return ""
+}
+
+func GetImage(username, imagename string) *image.LoadedImage {
+	imgman := getImageManager(username)
+	return imgman.GetImage(imagename)
+}
+
+func GetThumbnail(username, imagename string) *image.LoadedImage {
+	imgman := getImageManager(username)
+	return imgman.GetImage(imagename)
+}

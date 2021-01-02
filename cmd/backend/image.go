f9d8b9617b42daca7fbe7f3dea1cc7e64df07b7e
David Hein <david-hein196@web.de>
2021-01-02 16:14:19 +0100

    Change function signature to return Image instead of LoadedImage

--- cmd/backend/image.go
+++ cmd/backend/image.go
@@ -27,12 +27,12 @@ func UploadImage(user, name, creationDate string, raw []byte) string {
 	return ""
 }
 
-func GetImage(username, imagename string) *image.LoadedImage {
+func GetImage(username, imagename string) *image.Image {
 	imgman := getImageManager(username)
 	return imgman.GetImage(imagename)
 }
 
-func GetThumbnail(username, imagename string) *image.LoadedImage {
+func GetThumbnail(username string, start, length int) []*image.Image {
 	imgman := getImageManager(username)
-	return imgman.GetImage(imagename)
+	return *imgman.GetThumbnails(start, length)
 }

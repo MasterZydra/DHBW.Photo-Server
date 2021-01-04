bfed7dab24de14a8a56452f84ef841f07745336f
jones1008 <jones@fluhrers.de>
2021-01-04 19:33:44 +0100

    added auth documentation + todos

--- internal/user/auth.go
+++ internal/user/auth.go
@@ -5,8 +5,7 @@ import (
 	"strings"
 )
 
-// TODO: Jones Documentation
-
+// interface and type declarations for the abstraction used in the authentication wrapper functions
 type Authenticator interface {
 	Authenticate(user, password string, r *http.Request) bool
 }
@@ -17,6 +16,12 @@ func (af AuthenticatorFunc) Authenticate(user, password string, r *http.Request)
 	return af(user, password, r)
 }
 
+// The authentication wrapper AuthHandlerWrapper can be used as second argument for http.HandleFunc
+// It returns a function from type http.HandlerFunc.
+// Within this function the username and password is retrieved from the reqest object via BasicAuth().
+// These are passed to an Authenticate function from the interface of the first argument of AuthHandlerWrapper.
+// If this Authenticate function returns true, the handler (second argument) is executed.
+// If not it adds the Header "www-authenticate" to the response and writes the Status 401 Unauthorized.
 func AuthHandlerWrapper(authenticator Authenticator, handler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		usr, pw, ok := r.BasicAuth()
@@ -29,6 +34,9 @@ func AuthHandlerWrapper(authenticator Authenticator, handler http.HandlerFunc) h
 	}
 }
 
+// The AuthFileServerWrapper is basically the same as the AuthHandlerWrapper but returns a http.Handler
+// and executes the handler via ServeHTTP().
+// If the Autenticate function returned false it writes the status 403 Forbidden.
 func AuthFileServerWrapper(authenticator Authenticator, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		usr, pw, ok := r.BasicAuth()
@@ -40,11 +48,12 @@ func AuthFileServerWrapper(authenticator Authenticator, h http.Handler) http.Han
 	})
 }
 
+// The AuthHandler just returns the local AuthenticatorFunc authenticate.
 func AuthHandler() AuthenticatorFunc {
 	return authenticate
 }
 
-// führt authenticate aus und überprüft, ob die aktuell angefragte Datei zum Benutzer gehört
+// Executes authenticate and checks if the currently requested file (image) belongs to the current authenticated user.
 func AuthFileServer() AuthenticatorFunc {
 	return func(username, password string, r *http.Request) bool {
 		urlParts := strings.Split(strings.Trim(r.URL.Path, "/"), "/")
@@ -53,6 +62,7 @@ func AuthFileServer() AuthenticatorFunc {
 	}
 }
 
+// Gets the imageManager from cache and executes Authenticate with the given username and password.
 func authenticate(username, password string, r *http.Request) bool {
 	um := GetImageManager()
 	ok := um.Authenticate(username, password)

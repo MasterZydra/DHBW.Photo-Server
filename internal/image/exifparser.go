3f9f0d9b47414c268bd0a0392e14b26a8c5ca7fb
trigeha <51359838+trigeha@users.noreply.github.com>
2021-01-05 15:54:32 +0100

    Added Exif-Parser

---
+++ internal/image/exifparser.go
@@ -0,0 +1,72 @@
+package image
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"regexp"
+)
+
+func parseRawExifDataFromFile(file *os.File) ([]byte, error) {
+	// EXIF-Data is in APP1-Section
+	// Marker for APP1-Section: 0xFFE1
+	// read App1-Section data
+	app1Section, err := readSectionData(0xE1, bufio.NewReader(file))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return app1Section, nil
+}
+
+func readSectionData (marker byte, br *bufio.Reader) ([]byte, error) {
+	// find marker in reader and go there
+	var dataLength int = 0
+	var markerFound bool = false
+	for !markerFound {
+		c, err := br.ReadByte()
+		if err != nil {
+			return nil, err
+		} else if c == marker {
+			// marker found
+			markerFound = true
+			// first two bytes after marker are size of APP1-Section-Data
+			bytes := make([]byte, 2)
+			for k, _ := range bytes {
+				c, err := br.ReadByte()
+				if err != nil {
+					return nil, err
+				}
+				bytes[k] = c
+			}
+			// set dataLength
+			dataLength = int(binary.BigEndian.Uint16(bytes)) - 2
+		}
+	}
+
+	// read data
+
+	alreadyRead := 0
+	data := make([]byte, 0)
+
+	for alreadyRead < dataLength {
+		read := make([]byte, dataLength-alreadyRead)
+		n, err := br.Read(read)
+		alreadyRead += n
+		if err != nil && n < dataLength {
+			return nil, err
+		}
+		data = append(data, read[:n]...)
+	}
+
+	return data, nil
+}
+
+func getDateFromData (data []byte) []byte {
+	// DateTime Format: YYYY:MM:DD HH:MM:SS
+	dateRegex := regexp.MustCompile(`\d{4}\:(0[1-9]|1[012])\:(0[1-9]|[12][0-9]|3[01]) ([01][0-9]|2[0-3]):([0-6][0-9]):([0-6][0-9])`)
+	// There are three Tags with this format in EXIF-Data: DateTime, DateTimeOriginal, DateTimeDigitized
+	// all three Tags usually contain the same value
+	return dateRegex.Find(data)
+}

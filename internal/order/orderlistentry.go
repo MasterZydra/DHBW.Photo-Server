8843e6cb52b8f24fa6d2060406b970eb009d92cc
jones1008 <jones@fluhrers.de>
2021-01-07 11:30:01 +0100

    orderlistentry.go documentation

--- internal/order/orderlistentry.go
+++ internal/order/orderlistentry.go
@@ -2,7 +2,9 @@ package order
 
 import "DHBW.Photo-Server/internal/image"
 
-// TODO: jones: Documentation
+// ListEntry is used in the user.User object as the orderList.
+// It is used to represent one entry in that list.
+// It connects the entry with the associated image, it's desired print format and the number of prints.
 
 type ListEntry struct {
 	Image          *image.Image
